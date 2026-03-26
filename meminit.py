4805993321f93517953e0944516f7e3fa03a96a7
slaythespiredev <gwe37112@gmail.com>
2026-03-26 15:56:37 +0200

    Update meminit.py

--- meminit.py
+++ meminit.py
@@ -3,6 +3,11 @@ import time
 import keyboard
 import random
 import os
+import time 
+import math
+import date
+import datetime
+import sys
 
 print("PRE Post: Hello!")
 print("PRE Post: Please, wait a bit.")
